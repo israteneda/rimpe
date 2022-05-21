67944c82bde95e77f9b4121615e5c32922dfa8cb
Israel Teneda <israteneda@gmail.com>
2022-05-20 21:56:44 -0500

    add heroku config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python rimpe/app.py --port=$PORT
--- rimpe/app.py
+++ rimpe/app.py
@@ -1,6 +1,10 @@
 import tornado.ioloop
 import tornado.web
 from handlers import home
+from tornado.options import define, options, parse_command_line
+
+
+define("port", default="8000", help="Listening port", type=int)
 
 
 def make_app():
@@ -13,6 +17,7 @@ def make_app():
 
 
 if __name__ == "__main__":
+    parse_command_line()
     app = make_app()
-    app.listen(8888)
+    app.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
